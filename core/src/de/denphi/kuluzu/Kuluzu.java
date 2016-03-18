36b75d923fe0d6cec9635de4580c8c02b0976481
phil2105 <philipp_rabe@web.de>
2016-03-18 15:30:39 +0100

    new gamescreen class

--- core/src/de/denphi/kuluzu/Kuluzu.java
+++ core/src/de/denphi/kuluzu/Kuluzu.java
@@ -1,27 +1,22 @@
 package de.denphi.kuluzu;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class Kuluzu extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+import de.denphi.kuluzu.screens.Gamescreen;
+
+public class Kuluzu extends Game {
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		setScreen(new Gamescreen());
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 }
