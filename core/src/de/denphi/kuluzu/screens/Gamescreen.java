6dc63c4f31ba1e13eea3e721e80df18b17caa7b3
phil2105 <philipp_rabe@web.de>
2016-03-19 11:17:14 +0100

    new screens

--- core/src/de/denphi/kuluzu/screens/Gamescreen.java
+++ core/src/de/denphi/kuluzu/screens/Gamescreen.java
@@ -2,9 +2,13 @@ package de.denphi.kuluzu.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+import de.denphi.kuluzu.game.Level;
 
 /**
  * Created by Phil on 18.03.2016.
@@ -12,11 +16,15 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class Gamescreen implements Screen {
     SpriteBatch batch;
     Texture img;
+    ShapeRenderer shapeRenderer;
 
     @Override
     public void show() {
         batch = new SpriteBatch();
         img = new Texture("testbild.png");
+        Level l = new Level(4,8, new Texture("badlogic.jpg") );
+        l.createAnimals();
+        l.createAnwers();
     }
 
     @Override
@@ -26,6 +34,13 @@ public class Gamescreen implements Screen {
         batch.begin();
         batch.draw(img, 0, 0);
         batch.end();
+        shapeRenderer = new ShapeRenderer();
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
+        shapeRenderer.setColor(Color.BLACK);
+        shapeRenderer.rect(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight() * (float) 0.45);
+        shapeRenderer.rect(0, Gdx.graphics.getHeight()*(float) 0.45, Gdx.graphics.getWidth(), Gdx.graphics.getHeight()*(float) 0.45);
+
+        shapeRenderer.end();
     }
 
     @Override
